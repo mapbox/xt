9a3dc71d4686fc2d680d4f604db49e554fa56d6a
dnomadb <damon@mapbox.com>
2019-02-20 11:57:50 -0800

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.md', encoding='utf-8') as f:
 
 
 setup(name='xt',
-      version='0.1.0',
+      version='0.1.1',
       description=u"xForm Tile Coordinates",
       long_description=long_description,
       classifiers=[],
